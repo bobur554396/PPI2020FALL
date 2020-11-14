c6e69172422ace68b180c09131cb0be387c24083
bobur554396 <bobur.muhsimbaev@gmail.com>
2020-11-14 15:55:26 +0600

    STL; vector, set

---
+++ w10/G2/12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main(){
+  vector< vector<int> > v;
+
+  // 2 3 4
+  // 2 3 4 6
+  // 5 6
+  // 1 
+  // 1 2 3 4 5 6
+
+
+  return 0;
+}
